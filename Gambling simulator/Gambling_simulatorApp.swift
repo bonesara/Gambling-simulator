fa59c78872d1353085b8362cfcf26197e158c171
Rishi Ramanathan <ris.ramy@gmail.com>
2026-08-31 17:17:28 +0800

    Initial Commit

---
+++ Gambling simulator/Gambling_simulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gambling_simulatorApp.swift
+//  Gambling simulator
+//
+//  Created by Rishi on 31/8/26.
+//
+
+import SwiftUI
+
+@main
+struct Gambling_simulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
