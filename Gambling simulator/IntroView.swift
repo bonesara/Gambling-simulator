fa7f0e64cfab5242b25aa0646f34f2f6eb21c003
Rishi Ramanathan <ris.ramy@gmail.com>
2026-09-01 20:12:47 +0800

    I added an IntroView

---
+++ Gambling simulator/IntroView.swift
@@ -0,0 +1,18 @@
+//
+//  IntroView.swift
+//  Gambling simulator
+//
+//  Created by Rishi on 1/9/26.
+//
+
+import SwiftUI
+
+struct IntroView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IntroView()
+}
