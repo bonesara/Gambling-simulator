87df8fc3768df1cf6f7de718f057824ad762625a
Rishi Ramanathan <ris.ramy@gmail.com>
2026-09-01 21:44:18 +0800

    I changed the NavigationLink to fullScreenCover.

--- Gambling simulator/IntroView.swift
+++ Gambling simulator/IntroView.swift
@@ -10,8 +10,9 @@ import SwiftUI
 struct IntroView: View {
     @State var scale = 1.0
     @State var rotate = 0.0
+    @State private var showLaunch: Bool = false
+    
     var body: some View {
-        NavigationStack{
             VStack(spacing: 24){
                 Image(systemName: "die.face.5")
                     .scaledToFill()
@@ -21,20 +22,20 @@ struct IntroView: View {
                     .animation(.easeInOut(duration: 1), value: scale)
                 Text("WARM WELCOME TO GAMBLING SIMULATOR")
                 Text("Time to  put your money")
-                NavigationLink(){
-                    ContentView()
+                Button{
+                    showLaunch = true
                 } label: {
                     Text("So Lock In 🔏")
                         .foregroundColor(.green)
-                    //
-                    //            }
                 }
                 .navigationTitle("Helllllloooooo Customer!")
             }
             .onAppear {
                 scale = 1.5
-            }
         }
+            .fullScreenCover(isPresented: $showLaunch) {
+                ContentView()
+            }
     }
 }
 
